50a419959b18794aca3b218c368569eb47d3c8c0
Luke Winship <luke.winship@gmail.com>
2021-03-17 18:32:52 +0000

    fix: Link validateReq/Query in index file

--- built-types/index.d.ts
+++ built-types/index.d.ts
@@ -2,4 +2,10 @@ import { logger } from "./logger";
 import { kongSecretMiddleware } from "./kongSecretMiddleware";
 import postgres = require("./postgres");
 import { syncDbMigrations } from "./syncDbMigrations";
+import { validateReq } from "./expressUtils";
+import { validateReqQuery } from "./expressUtils";
+export declare namespace expressUtils {
+    export { validateReq };
+    export { validateReqQuery };
+}
 export { logger, kongSecretMiddleware, postgres, syncDbMigrations };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ const { logger } = require('./logger');
 const { kongSecretMiddleware } = require('./kongSecretMiddleware');
 const postgres = require('./postgres');
 const { syncDbMigrations } = require('./syncDbMigrations');
+const { validateReq, validateReqQuery } = require('./expressUtils');
 
 module.exports = {
   logger,
   kongSecretMiddleware,
   postgres,
   syncDbMigrations,
+  expressUtils: {
+    validateReq,
+    validateReqQuery,
+  },
 };
