b9d4b027e6538f252e9ec89b5f5ea2bbb3d4796c
Luke Winship <luke.winship@gmail.com>
2021-08-16 17:43:54 +0100

    .

--- built-types/syncDbMigrations.d.ts
+++ built-types/syncDbMigrations.d.ts
@@ -8,19 +8,39 @@ export type PostgresConnectionDetails = {
     isRds?: boolean | null | undefined;
 };
 /**
+ * See: https://db-migrate.readthedocs.io/en/latest/API/programable/#:~:text=cmdoptions
+ */
+export type DbMigrateCmdOptions = {
+    [k: string]: any;
+    'migrations-dir': string;
+};
+/**
  * @typedef {import('./postgres').PostgresConnectionDetails} PostgresConnectionDetails
+ */
+/**
+ * @typedef {{
+ *   'migrations-dir': string,
+ *   [k: string]: any,
+ * }} DbMigrateCmdOptions
+ *   See: https://db-migrate.readthedocs.io/en/latest/API/programable/#:~:text=cmdoptions
  */
 /**
  * @param {object} [options]
  * @param {PostgresConnectionDetails} [options.postgresConnection] If excluded, defaults to using, from environment vars:
  *   `POSTGRES_USER`, `POSTGRES_PASSWORD`, `POSTGRES_HOST`, `POSTGRES_DB`, `POSTGRES_APP_NAME`.
  * @param {boolean} [options.doStartDummyExpressServer] Defaults to `true`
+ * @param {DbMigrateCmdOptions} [options.dbMigrateCmdOptions]
  */
 export function syncDbMigrations(options?: {
     postgresConnection?: import("./postgres").PostgresConnectionDetails | undefined;
     doStartDummyExpressServer?: boolean | undefined;
+    dbMigrateCmdOptions?: DbMigrateCmdOptions | undefined;
 } | undefined): Promise<void>;
 /**
  * @param {PostgresConnectionDetails} [maybePostgresConnection]
+ * @param {{ 'migrations-dir': string, [k: string]: any }} [maybeDbMigrateCmdOptions]
  */
-export function getDbMigrateInstance(maybePostgresConnection?: import("./postgres").PostgresConnectionDetails | undefined): Promise<any>;
+export function getDbMigrateInstance(maybePostgresConnection?: import("./postgres").PostgresConnectionDetails | undefined, maybeDbMigrateCmdOptions?: {
+    [k: string]: any;
+    'migrations-dir': string;
+} | undefined): Promise<any>;
